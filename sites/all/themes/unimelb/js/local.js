4c5eff846cc9ac5b22252d6e2e691a55fb450e7f
Peter Lieverdink <peter.lieverdink@unimelb.edu.au>
2012-05-29 09:03:35 +1000

    Only set the title attribute on the arrow, not whe whole link.

--- sites/all/themes/unimelb/js/local.js
+++ sites/all/themes/unimelb/js/local.js
@@ -3,10 +3,10 @@
   Drupal.behaviors.unimelb = {
     attach: function(context, settings) {
 
-      $('ul.jquerymenu-processed li.parent.closed').attr('title', 'Expand this menu item');
-      $('ul.jquerymenu-processed li.parent.open').attr('title', 'Collapse this menu item');
+      $('ul.jquerymenu-processed li.parent span.closed').attr('title', 'Expand this menu item');
+      $('ul.jquerymenu-processed li.parent span.open').attr('title', 'Collapse this menu item');
 
-      $('ul.jquerymenu-processed li.parent').bind('click', function() {
+      $('ul.jquerymenu-processed li.parent span').bind('click', function() {
         if ($(this).is('.open')) {
           $(this).attr('title', 'Collapse this menu item');
         }
