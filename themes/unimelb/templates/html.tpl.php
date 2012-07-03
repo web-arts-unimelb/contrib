ca9ba49305a62b366927bfab1ec69c84cb065a8c
Peter Lieverdink <peter.lieverdink@unimelb.edu.au>
2012-07-03 10:07:42 +1000

    Close the html tag properly if $rdf_namespaces is (somehow) empty.

--- themes/unimelb/templates/html.tpl.php
+++ themes/unimelb/templates/html.tpl.php
@@ -45,7 +45,7 @@
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print ($rdf_namespaces) ? $rdf_namespaces : ">"; ?>
 <head>
 
 <?php print $head; ?>
