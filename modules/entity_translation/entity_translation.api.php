1041fee3c86518309688415798563037fe018433
Peter Lieverdink <peter.lieverdink@unimelb.edu.au>
2012-08-03 15:47:08 +1000

    Add updated entity_translation module.

--- modules/entity_translation/entity_translation.api.php
+++ modules/entity_translation/entity_translation.api.php
@@ -2,37 +2,43 @@
 
 /**
  * @file
- * API documentation for Entity Translation module.
+ * API documentation for the Entity translation module.
  */
 
 /**
- * Allow modules to define their own translation info.
+ * Allows modules to define their own translation info.
+ *
+ * @param $types
+ *   The available entity types.
  *
  * @return
  *   An array of entity translation info to be merged into the entity info.
  *   The translation info is an associative array that has to match the
- *   following sample structure:
- *   @code
- *   array(
- *     // Three nested sub-arrays keyed respectively by entity type and the
- *     // 'translation' keys: the first one is the key defined by the core
- *     // entity system, while the nested one registers Translation as a
- *     // translation handler.
- *     'custom_entity' => array(
- *       'translation' => array(
- *         'translation' => array(
- *           'class' => the entity class name,
- *           'base path' => the base menu path to which attach the administration UI,
- *           'access callback' => the access callback for the admin pages,
- *           'access arguments' => the access arguments,
- *           // The edit form information, used to add the retranslate checkbox
- *           // to the entity edit form (if empty the edit form will not be
- *           // altered by Translation).
- *           'edit form' => TRUE,
- *         ),
- *       ),
- *     ),
- *   );
+ *   following structure. Three nested sub-arrays keyed respectively by entity
+ *   type, the 'translation' key and the 'entity_translation' key: the second
+ *   one is the key defined by the core entity system while the third one
+ *   registers Entity translation as a field translation handler. Elements:
+ *   - class: The name of the translation handler class, which is used to handle
+ *     the translation process. Defaults to 'EntityTranslationDefaultHandler'.
+ *   - base path: The base menu router path to which attach the administration
+ *     user interface. Defaults to "$entity_type/%$entity_type".
+ *   - access callback: The access callback for the translation pages. Defaults
+ *     to 'entity_translation_tab_access'.
+ *   - access arguments: The access arguments for the translation pages.
+ *     Defaults to array($entity_type).
+ *   - view path: The menu router path to be used to view the entity. Defaults
+ *     to the base path.
+ *   - edit path: The menu router path to be used to edit the entity. Defaults
+ *     to "$base_path/edit".
+ *   - path wildcard: The menu router path wildcard identifying the entity.
+ *     Defaults to "%$entity_type".
+ *   - theme callback: The callback to be used to determine the translation
+ *     theme. Defaults to 'variable_get'.
+ *   - theme arguments: The arguments to be used to determine the translation
+ *     theme. Defaults to array('admin_theme').
+ *   - edit form: The key to be used to retrieve the entity object from the form
+ *     state array. An empty value prevents Entity translation from performing
+ *     alterations to the entity form. Defaults to $entity_type.
  */
 function hook_translation_info($types = NULL) {
   $info['custom_entity'] = array(
@@ -42,7 +48,7 @@ function hook_translation_info($types = NULL) {
         'base path' => 'custom_entity/%custom_entity',
         'access callback' => 'custom_entity_tab_access',
         'access arguments' => array(1),
-        'edit form' => TRUE,
+        'edit form' => 'custom_entity_form_key',
       ),
     ),
   );
@@ -51,19 +57,44 @@ function hook_translation_info($types = NULL) {
 }
 
 /**
- * Allow modules to react on translation events.
+ * Allows modules to act when a new translation is added.
  *
- * @param string $entity_type
- *   The type of entity; e.g. 'node' or 'user'.
- * @param object $entity
- *   The entity to be translated.
- * @param string $langcode
- *   The language code of the translation.
+ * @param $entity_type
+ *   The entity type.
+ * @param $entity
+ *   The entity.
+ * @param $translation
+ *   The inserted translation array.
+ * @param $values
+ *   The translated set of values, if any.
  */
-function hook_entity_translation_save($entity_type, $entity, $langcode) {
-  $function = 'pathauto_' . $entity_type . '_update_alias';
-  if (function_exists($function)) {
-    $options = array('language' => $langcode);
-    $function($entity, 'update', $options);
-  }
+function hook_entity_translation_insert($entity_type, $entity, $translation, $values = array()) {
+}
+
+/**
+ * Allows modules to act when a translation is updated.
+ *
+ * @param $entity_type
+ *   The entity type.
+ * @param $entity
+ *   The entity.
+ * @param $translation
+ *   The updated translation array.
+ * @param $values
+ *   The translated set of values, if any.
+ */
+function hook_entity_translation_update($entity_type, $entity, $translation, $values = array()) {
+}
+
+/**
+ * Allows modules to act when a translation is deleted.
+ *
+ * @param $entity_type
+ *   The entity type.
+ * @param $entity
+ *   The entity.
+ * @param $langcode
+ *   The langcode of the translation which was deleted.
+ */
+function hook_entity_translation_delete($entity_type, $entity, $langcode) {
 }
