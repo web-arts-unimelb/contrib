cf69ca1556d0eab549a4d1e31b49964a0d3ddc18
arts-webmaster <web.arts.unimelb@gmail.com>
2012-09-11 11:11:24 +1000

    academic staff self edit profile's style is fixed now

--- themes/unimelb/js/local.js
+++ themes/unimelb/js/local.js
@@ -15,22 +15,65 @@
 			});
 
 			/* Start academic self profile editing */
+			__adjust_uploader();
 			
-			/* Start account document uploader */
-			var tag_name = "fieldset#edit-field-account-document-und";
-			__adjust_uploader_style(tag_name);			
-			/* End account document uploader */
-
-			/* Start account document uploader */
-			var tag_name = "fieldset#edit-field-account-image-und"; 
-			__adjust_uploader_style(tag_name);
-			/* End account document uploader */			
-
+			__adjust_group();
+			__adjust_ungroup()
 			/* End academic self profile editing */
+
 		}
 		
 	}
 })(jQuery);
+
+
+// Define your functions here
+function __adjust_uploader()
+{
+	/* Account document uploader */
+	var tag_name = "fieldset#edit-field-account-document-und";
+	__adjust_uploader_style(tag_name);			
+
+	/* Account document uploader */
+	var tag_name = "fieldset#edit-field-account-image-und"; 
+	__adjust_uploader_style(tag_name);
+}
+
+function __adjust_group()
+{
+	var tag_name = "fieldset.group-research-interest";
+	__adjust_group_style(tag_name);
+
+	var tag_name = "fieldset.group-biography";
+	__adjust_group_style(tag_name);
+
+	var tag_name = "fieldset.group-research-projects";
+	__adjust_group_style(tag_name);
+
+	var tag_name = "fieldset.group-publications";
+	__adjust_group_style(tag_name);
+
+	var tag_name = "fieldset.group-teaching";
+	__adjust_group_style(tag_name);
+
+	var tag_name = "fieldset.group-awards-and-grants";
+	__adjust_group_style(tag_name);
+
+	var tag_name = "fieldset.group-presentations";
+	__adjust_group_style(tag_name);
+
+	var tag_name = "fieldset.group-bibliography";
+	__adjust_group_style(tag_name);
+}
+
+function __adjust_ungroup()
+{
+	var tag_name = "div#edit-field-qualifications";
+	__adjust_ungroup_style(tag_name);
+
+	var tag_name = "div#edit-field-other-information";
+	__adjust_ungroup_style(tag_name);
+}
 
 
 function __adjust_uploader_style(tag_name)
@@ -39,4 +82,25 @@ function __adjust_uploader_style(tag_name)
 	$(tag_name).css("border", "0px");
 	$(tag_name).removeClass("collapsible");
 
+}
+
+function __adjust_group_style(tag_name)
+{
+	var width = "650px";
+	
+	$(tag_name).width(width);
+	$(tag_name).removeClass("collapsible");
+	$(tag_name).removeClass("collapsed");
+	
+	$(tag_name).css("background", "#ffffff");	
+	$(tag_name).css("border", "0px");	
+	$(tag_name).css("margin-bottom", "-10px");
+}
+
+function __adjust_ungroup_style(tag_name)
+{
+	var width = "650px";
+	
+	$(tag_name).width(width);
+	$(tag_name).css("padding-top", "10px");
 }
