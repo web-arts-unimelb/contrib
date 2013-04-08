368eb33434480e892c444eefe8bcade134f9c54c
arts-webmaster <web.arts.unimelb@gmail.com>
2013-04-08 20:15:00 +1000

    further fix input issue for site_survey

--- modules/custom/site_survey/js/local.js
+++ modules/custom/site_survey/js/local.js
@@ -47,6 +47,7 @@ jQuery(document).ready(function() {
 		alert('Survey cookie cleared. Hit Refresh to see the survey again.');
 	});
 
+	//
 	jQuery("input[name='visitor_type']").click(function() {
 		if( jQuery('input:radio[name=visitor_type]:checked').val() === 'Other' )
 		{
@@ -57,6 +58,13 @@ jQuery(document).ready(function() {
 			 jQuery("input[name='other_visitor_type']").attr("value", "");
 		}
 	});
+
+	//
+	jQuery("input[name='other_visitor_type']").click(function() {
+		jQuery("input[name='visitor_type']").removeAttr("checked");
+		jQuery("input[value='Other']").attr("checked", true);
+	});
+
 
 	checkCookie('site_survey_cookie');
 	assign_visitor_ip();	
